29f55a67842267c5a1a92d1a63682c4da3078741
Adepitan <you@example.com>
2022-09-16 10:48:59 +0200

    completed food choice section

---
+++ starter/main.js
@@ -0,0 +1,16 @@
+// basic ES5 function diclearation
+var myname="Mayorkeyz"
+function popsomething(){
+    alert(myname +" has been coding since 12")
+}
+console.log(document)
+var ourbody = document.querySelector(".ourpagebody")
+// ourbody.style.background = "orange"
+
+var harmburger =document.querySelector(".harmburger-container")
+console.log(harmburger)
+
+
+function addandremove(){
+    harmburger.classList.toggle("showburgar")
+}
